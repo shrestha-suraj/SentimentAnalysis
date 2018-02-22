c6c747460faa93531756656a591148d425b9ffa4
Suraj Shrestha <sshrest4@go.olemiss.edu>
2018-02-22 11:45:11 -0600

    The final program is done but still it is a little bit slow in processing. Need to improve the Non functional Requirement of the project.

--- SilverPuffin.py
+++ SilverPuffin.py
@@ -1,24 +1,41 @@
+# Spirit Animal User ID: SilverPuffin
+# Date the file was last edited: Feb 22, 2018
+# Challenge Number: 2.0
+
+#  Importing contents to be used in the project
 from __future__ import division
 import numpy as np
 import matplotlib.pyplot as mplot
 import re
+import os
 
+# Analysing Lexicon file to split the words and its emotional value
 lexicon_file=open("sentiment_lex.csv","r")
 lexicon_file=lexicon_file.read()
 lexicon_file=lexicon_file.split("\n")
+#<-------------------------------------------------------------------------------------->
+# The function of mergeSeries is tokae in a String 'a' or 'b' from the user and then
+# use the algorithm to search text files with specific starting charcater i.e. 'a' or 'b'
+# anc concadinate its string as a whole in a variable main_string and call the function
+# scoreAnalysis(main_string) for printing the graph.
+def mergeSeries (series):
+    main_string=""
+    path="./"  
+    dirList=os.listdir(path)
+    for filename in dirList:
+        if filename.startswith(series.lower()):
+            file=open(filename,"r")
+            file=file.read()
+            main_string+=file
+    scoreAnalysis(main_string)
 
+#<-------------------------------------------------------------------------------------->    
 
-
-def takeInScript(file_name):
-    file=open (file_name,"r")
-    file=file.read()
-    file=re.sub(r'\W+'," ",file)
+def scoreAnalysis(huge_string):
+    file=re.sub(r'\W+'," ",huge_string)
     file=file.split(" ")
     file=list(set(file))
-    scoreAnalysis(file)
-    
-def scoreAnalysis(file):
-    dictionary={"Neg": 0.0, "W.Neg": 0.0, "Neu": 0.0, "W.Pos": 0.0, "Pos": 0.0}
+    dictionary={"Neg": 0, "W.Neg": 0, "Neu": 0, "W.Pos": 0, "Pos": 0}
     clean_text=[]
     for x in file:
         if not x.isalpha() or len(x)==1 or x==x.upper():
@@ -29,15 +46,15 @@ def scoreAnalysis(file):
             lexicon_data=y.split(",")
             if x.lower()==lexicon_data[0]:
                 if -1.0<=np.float32(lexicon_data[1])<-0.6:
-                    dictionary["Neg"]+=np.float32(lexicon_data[1])
+                    dictionary["Neg"]+=1
                 elif -0.6<=np.float32(lexicon_data[1])<-0.2:
-                    dictionary["W.Neg"]+=np.float32(lexicon_data[1])
+                    dictionary["W.Neg"]+=1
                 elif -0.2<=np.float32(lexicon_data[1])<=0.2:
-                    dictionary["Neu"]+=np.float32(lexicon_data[1])
+                    dictionary["Neu"]+=1
                 elif 0.2<np.float32(lexicon_data[1])<=0.6:
-                    dictionary["W.Pos"]+=np.float32(lexicon_data[1])
+                    dictionary["W.Pos"]+=1
                 elif 0.6<=np.float32(lexicon_data[1])<=1.0:
-                    dictionary["Pos"]+=np.float32(lexicon_data[1])
+                    dictionary["Pos"]+=1
     values=dictionary.values()
     keys=dictionary.keys()
     mplot.bar(range(0,len(values)),np.log10(values))
@@ -45,9 +62,9 @@ def scoreAnalysis(file):
     mplot.xlabel("Word Range")
     mplot.ylabel("Number of words")
     mplot.show()
-                
-takeInScript("a101script.txt")
-takeInScript("bg101script.txt")
 
-#print clean_text
+#<--------------------------------------------------------------------------------------->
+    
+mergeSeries(raw_input ("Enter the series name (a or b): "))
+
             
